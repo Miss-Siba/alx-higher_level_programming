413b4fbae81331fbe18e4a12b64b12cae1c59871
Miss-Siba <sibaquma@gmail.com>
2023-11-30 04:58:35 -0800

    modules

---
+++ 0x02-python-import_modules/2_arg.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+
+def print_arguments():
+    num_arguments = len(sys.argv) - 1
+
+    if num_arguments == 1:
+        print(f"{num_arguments} argument:")
+    elif num_arguments == 0:
+        print("0 arguments.")
+    else:
+        print(f"{num_arguments} arguments:")
+    for i in range(1, num_arguments + 1):
+        print(f"{i} {sys.argv[i]}")
+
+
+if __name__ == "__main__":
+    print_arguments()
