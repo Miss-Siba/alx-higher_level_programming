36243fc9d667853c9ed22a56ca345444049fc70d
Miss-Siba <sibaquma@gmail.com>
2023-12-06 10:41:31 -0800

    more_data_structures

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,3 @@
 def update_dictionary(a_dictionary, key, value):
-    if key in a_dictionary:
         a_dictionary[key] = value
-        print(value)
-    else:
-        a_dictionary[key] = value
-        print(a_dictionary[key])
+        return (a_dictionary)
