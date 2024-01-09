356602259ff681bd7889c83e306572afc4607134
Miss-Siba <sibaquma@gmail.com>
2024-01-09 12:36:17 -0800

    input

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,11 +4,5 @@
 
 def read_file(filename=""):
     """Reads a text file and prints to stdout."""
-    try:
-        with open(filename, 'r', encoding='UTF8') as file:
-            content = file.read()
-            print(content)
-    except FileNotFoundError:
-        print(f"File '{filename}' not found.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read(), end="")
