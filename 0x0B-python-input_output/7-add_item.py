d65a14237c348cbb72eb8c9872f19bbdab113104
Miss-Siba <sibaquma@gmail.com>
2024-01-10 08:00:30 -0800

    input

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 
 """Add all arguments to a Python list and save them to a file."""
-
 import sys
-from importlib import import_module
-
-save_to_json_file_module = import_module('5-save_to_json_file')
-load_from_json_file_module = import_module('8-load_from_json_file')
-
 
 if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
     try:
         items = load_from_json_file("add_item.json")
     except FileNotFoundError:
         items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -13,11 +13,10 @@ class Student:
             last_name (str): The last name of the student.
             age (int): The age of the student.
         """
-
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
-          """Get a dictionary representation of the Student."""
-        return self.
+        """Get a dictionary representation of the Student."""
+        return self.__dict__
