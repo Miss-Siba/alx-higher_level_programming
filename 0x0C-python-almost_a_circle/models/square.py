e5e2fa9f0f9eefed7f46ccbedc51fc7a47af0145
Miss-Siba <sibaquma@gmail.com>
2024-01-15 11:06:03 -0800

    models

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,6 +5,7 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """ Represents asquare."""
+
     def __init__(self, size, x=0, y=0, id=None):
         """Initializes a new square
         Args:
