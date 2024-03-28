7d05aa461dee9d485e5265b69c83a85964a89f24
Miss-Siba <sibaquma@gmail.com>
2024-03-28 09:44:13 -0700

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Script takes in a URL, sends a request to that URL and dispalays the size of the body of the response.
-curl -sI "$1" | grep "Content Length" | wc -c
+curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
