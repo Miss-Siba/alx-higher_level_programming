e3270c2b43e6d056f52d05b6dbdd0be900c39a1a
Miss-Siba <sibaquma@gmail.com>
2024-03-29 07:43:51 -0700

    task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,18 +1,40 @@
 #!/usr/bin/python3
+"""
+Script to send a POST request with an email parameter to a specified URL.
+"""
 import urllib.parse
 import urllib.request
 import sys
 
-# Get the URL and email from command-line arguments
-url = sys.argv[1]
-email = sys.argv[2]
 
-# Encode the email parameter
-data = urllib.parse.urlencode({'email': email})
-data = data.encode('utf-8')
+def send_post_request(url, email):
+    """
+    Sends a POST request with the specified email parameter to the given URL.
 
-req = urllib.request.Request(url, data=data, method='POST')
-with urllib.request.urlopen(req) as response:
-    content = response.read().decode('utf-8')
+    Parameters:
+    - url (str): The URL to send the POST request to.
+    - email (str): The email parameter to include in the request.
 
-print(content)
+    Returns:
+    - str: The body of the response (decoded in utf-8).
+    """
+
+    data = urllib.parse.urlencode({'email': email})
+    data = data.encode('utf-8')
+
+    req = urllib.request.Request(url, data=data, method='POST')
+    with urllib.request.urlopen(req) as response:
+        content = response.read().decode('utf-8')
+        return content
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: python script_name.py <URL> <email>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    response_body = send_post_request(url, email)
+    print(response_body)
