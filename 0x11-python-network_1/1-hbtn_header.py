d00ec3c496ade060da7d783eddee9b3881f582f5
Miss-Siba <sibaquma@gmail.com>
2024-03-29 07:20:28 -0700

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,17 +8,18 @@ def get_x_request_id(url):
     """ Gets and displays x_request_id """
     with urllib.request.urlopen(url) as response:
         headers = response.info()
-        request_id = headers.get('X-Request-Id')
-        return request_id if request_id else None
+        return headers.get('X-Request-Id')
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: python get_request_id.py <URL>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    x_request_id = get_x_request_id(url)
+
+    if x_request_id:
+        print(x_request_id)
     else:
-        url = sys.argv[1]
-        x_request_id = get_x_request_id(url)
-        if x_request_id:
-            print(x_request_id)
-        else:
-            print("X-Request-Id header not found in the response.")
+        print("X-Request-Id header not found in the response.")
