3bf855bf8b3b1a39fa43b5f7b3a2b5ded675dcc1
Miss-Siba <sibaquma@gmail.com>
2024-05-02 21:25:40 +0200

    updates

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,2 @@
+// Update text color of the HTML tag header to red
+document.querySelector('header').style.color = '#FF0000';
---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,5 @@
+// Updates the text color of the header to red.
+
+$('div#red_header').click(function () {
+    $('header').css('color', '#FF0000');
+  });
